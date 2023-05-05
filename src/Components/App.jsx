8b707dc52806d6261026d8baaef20bef263ad43b
danielablasco <daniielablasco090@gmail.com>
2023-05-05 17:56:27 -0300

    se envio todo

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Paginas from "./Paginas";
+function App() {
+  return (
+    <div className="App">
+     <h1>Hola mundo</h1> 
+     <Paginas></Paginas>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Paginas.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ImgLogo1 from "../images/logoCherry1.jpeg";
+const Paginas = () => {
+  return (
+    <div>
+      <div>este es el componente paginas</div>
+
+      <img
+        src={ImgLogo1}
+      >
+      
+      </img>
+    </div>
+  );
+};
+
+export default Paginas;
